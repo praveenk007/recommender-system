d2e7b2cf3cffd7031f4d58123d16bb2de4c1cd79
pk1992 <praveen.k@gmail.com>
2018-09-15 23:12:05 +0530

    refactored

--- app/algo/Recommenders.py
+++ app/algo/Recommenders.py
@@ -1,7 +1,5 @@
-import pandas as pd
-import math
 
-class popularity_recommender():
+class PopularityRecommend:
 
     def __init__(self):
         self.train_data = None
@@ -30,10 +28,7 @@ class popularity_recommender():
         return user_recommendations
 
 
-
-
-
-class similar_recommender():
+class SimilarRecommend:
 
     def __init__(self):
         self.train_data = None
@@ -56,7 +51,6 @@ class similar_recommender():
         return self.similarity_model
 
     def create_model_v2(self, train_data, user_id, item_id, feature):
-        print(train_data)
         self.user_id = user_id
         self.item_id = item_id
         train_data['feature'] = train_data[feature]
@@ -74,79 +68,6 @@ class similar_recommender():
 
         return [];
 
-    def correlate(self, train_data_grouped, user_id, item_id, feature, weight):
-        """
-        finds correlation between users
-        :param item_id:
-        :param train_data_grouped:
-        :return:
-        """
-        unique_users = self.get_unique_objects(train_data_grouped, user_id)
-        unique_items = self.get_unique_objects(train_data_grouped, item_id)
 
-        user_item_matrix = pd.DataFrame(index=unique_items, columns=unique_users)
 
-        for user in unique_users:
-            for item in unique_items:
-                temp_df = train_data_grouped.query(user_id + ' == @user and ' + item_id + '== @item').reset_index()
-                if temp_df.empty:
-                    user_item_matrix[user][item] = 0
-                else:
-                    user_item_matrix[user][item] = temp_df[feature + '_score'].iloc[0] * weight
-        print(user_item_matrix)
 
-        corr_matrix = pd.DataFrame(index=unique_users, columns=unique_users)
-
-        for user1 in unique_users:
-            user1_df = user_item_matrix[user1].astype(str).astype(float)
-            user1_nonzero_count = (user1_df != 0).sum()
-            user1_nonzero_plans = user1_df.loc[user1_df != 0].index.tolist()
-            user1_mean = user1_df.sum() / user1_nonzero_count
-            for user2 in unique_users:
-                if user1 == user2:
-                    corr_matrix[user1][user2] = 1
-                    if user1_nonzero_count <= 1:
-                        break
-                    else:
-                        continue
-                user2_df = user_item_matrix[user2]
-                user2_nonzero_count = (user2_df != 0).sum()
-                if user2_nonzero_count <= 1:
-                    continue
-                user2_mean = user2_df.sum() / user2_nonzero_count
-                user2_nonzero_plans = user2_df.loc[user2_df != 0].index.tolist()
-                common_plans = self.find_common_plans(user1_nonzero_plans, user2_nonzero_plans)
-                if len(common_plans) <= 1:
-                    continue
-                print user1, user2
-                print common_plans
-                num_summation = 0
-                user1item_minus_mean_sqr_summation = 0
-                user2item_minus_mean_sqr_summation = 0
-                for item in common_plans:
-                    if user_item_matrix[user1][item] != 0 and user_item_matrix[user2][item] != 0:
-                        user1_score = user_item_matrix[user1][item]
-                        user2_score = user_item_matrix[user2][item]
-
-                        user1item_minus_mean = user1_score - user1_mean
-                        user2item_minus_mean = user2_score - user2_mean
-
-                        user1item_minus_mean_sqr_summation = user1item_minus_mean_sqr_summation + (user1item_minus_mean * user1item_minus_mean)
-                        user2item_minus_mean_sqr_summation = user2item_minus_mean_sqr_summation + (user2item_minus_mean * user2item_minus_mean)
-
-                        num_summation = num_summation + (user1item_minus_mean * user2item_minus_mean)
-                if num_summation == 0:
-                    corr_matrix[user1][user2] = 0
-                else:
-                    corr_matrix[user1][user2] = num_summation / (
-                        math.sqrt(user1item_minus_mean_sqr_summation * user2item_minus_mean_sqr_summation)
-                    )
-        print corr_matrix
-
-    def find_common_plans(self, user1_nonzero_plans, user2_nonzero_plans):
-        if len(user1_nonzero_plans) > len(user2_nonzero_plans):
-            return [plan for plan in user1_nonzero_plans if plan in user2_nonzero_plans]
-        return [plan for plan in user2_nonzero_plans if plan in user1_nonzero_plans]
-
-    def get_unique_objects(self, data, ids):
-        return data[ids].unique()
--- app/services/recommender_service.py
+++ app/services/recommender_service.py
@@ -1,5 +1,6 @@
 import pandas
 from app.algo import Recommenders
+from app.algo import Correlation
 from app.dao import Dao
 from bson.json_util import loads
 from sklearn.model_selection import train_test_split
@@ -8,9 +9,9 @@ def visualize_data():
     all_data = pandas.read_csv('dataset/user_activity.txt')
 
     all_data_grouped = all_data.groupby(['plan','state']).agg({'count' : 'count'}).reset_index()
-    gropued_count = all_data_grouped['count'].sum()
-    print(gropued_count)
-    all_data_grouped['percentage'] = all_data_grouped['count'].div(gropued_count) * 100
+    grouped_count = all_data_grouped['count'].sum()
+    print(grouped_count)
+    all_data_grouped['percentage'] = all_data_grouped['count'].div(grouped_count) * 100
 
 
     print(all_data_grouped)
@@ -18,7 +19,7 @@ def visualize_data():
 def create_model_v1(mongo):
     all_data = pandas.DataFrame.from_dict(Dao.mongo_data_dao().find_all(mongo))
     train_data, test_data = train_test_split(all_data, test_size=0.10, random_state=0)
-    similarity_reco = Recommenders.similar_recommender()
+    similarity_reco = Recommenders.SimilarRecommend()
     op = similarity_reco.create_model_v2(train_data, 'user', 'plan', ['plan', 'state'])
     Dao.mongo_model_dao().drop(mongo)
     Dao.mongo_model_dao().persist_many(mongo, loads(op.to_json(orient='records')))
@@ -27,10 +28,11 @@ def create_model_v1(mongo):
 def create_model_v2(mongo, request_json):
     all_data = pandas.DataFrame.from_dict(Dao.mongo_data_dao().find_all(mongo))
     train_data, test_data = train_test_split(all_data, test_size=0.10, random_state=0)
-    similarity_reco = Recommenders.similar_recommender()
+    similarity_reco = Recommenders.SimilarRecommend()
     reco_meta = Dao.mongo_reco_meta_dao().getOne(mongo, request_json['id'])
     train_data_grouped = similarity_reco.create_model_v2(train_data, reco_meta['user_field'], reco_meta['item_field'], reco_meta['features'][0]['id'])
-    similarity_reco.correlate(train_data_grouped, reco_meta['user_field'], reco_meta['item_field'], reco_meta['features'][0]['id'], reco_meta['features'][0]['weight'])
+
+    Correlation.PearsonCorrelation().correlate(train_data_grouped, reco_meta['user_field'], reco_meta['item_field'], reco_meta['features'][0]['id'], reco_meta['features'][0]['weight'])
 
 
 def recommend_popular(mongo):
@@ -39,7 +41,7 @@ def recommend_popular(mongo):
 def recommend_similar(mongo):
     all_data = pandas.read_json('dataset/user_activity.json')
     train_data, test_data = train_test_split(all_data, test_size = 0.10, random_state=0)
-    popularity_reco = Recommenders.popularity_recommender()
+    popularity_reco = Recommenders.PopularityRecommend()
     popularity_reco.create_model(train_data, 'user', 'plan', ['plan', 'state'])
     users = test_data['user'].unique()
     return popularity_reco.recommend(users[0])
